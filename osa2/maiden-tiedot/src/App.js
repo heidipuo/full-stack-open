955bf51f165ba9dee5b8fd0f7dd00db296da999e
Heidi <puotiniemi.heidi@gmail.com>
2023-08-07 14:31:49 +0300

    maatiedot little fix

--- osa2/maiden-tiedot/src/App.js
+++ osa2/maiden-tiedot/src/App.js
@@ -34,27 +34,6 @@ const Display = (props) => {
     )
   }
 }
-/*
-const Country = (props) => {
-  console.log('info', props.country)
-  return(
-    <div>
-      <h1>{props.country.name.common}</h1> 
-        <p>capital {props.country.capital}</p>
-        <p>area {props.country.area}</p>
-      <h2>Languages</h2>
-      <ul>
-          {Object.values(props.country.languages).map((language, i) => (
-            <li key={i}>{language}</li>
-          ))}
-        </ul>
-        <img src={props.country.flags.png} alt={`${props.country.name.common} flag`} />  
-        <Weather city={props.country.capital}/>
-    </div>
-    
-  )
-}
-*/
 
 const App = () => {
   const[newCountry, setNewCountry] = useState('')
