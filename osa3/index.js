99cdda9d94fc075c3145a17d358dc1933899a9f3
Heidi <puotiniemi.heidi@gmail.com>
2023-08-11 13:44:57 +0300

    puhelinluettelo full stack done

--- osa3/index.js
+++ osa3/index.js
@@ -101,7 +101,9 @@ app.post('/api/persons', (request, response) => {
   })
 
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`) 
 })
+
+   //"build:ui": "rm -rf build && cd cd ../osa2/puhelinluettelo/ && npm run build && cp -r build ../../osa3",
