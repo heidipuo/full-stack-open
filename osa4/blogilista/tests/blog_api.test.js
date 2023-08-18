b4f844603c36237d09d2bc5fb2bf097ac0e97fd4
Heidi <puotiniemi.heidi@gmail.com>
2023-08-18 11:17:38 +0300

    bloglist tests parts 4.9-4.11 done

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -8,32 +8,91 @@ const initialBlogs = require('../test_blogs.json')
 
 beforeEach(async () => {
   await Blog.deleteMany({})
-  let blogObject = new Blog(initialBlogs[0])
-  await blogObject.save()
-  blogObject = new Blog(initialBlogs[1])
-  await blogObject.save()
+  await Blog.insertMany(initialBlogs)
 })
 
-test('notes are returned as json', async () => {
+test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
 })
 
-test('there are two blogs', async () => {
+test('the amount of blogs is correct', async () => {
   const response = await api.get('/api/blogs')
 
   expect(response.body).toHaveLength(initialBlogs.length)
 })
 
-test('the first blog called Moi', async () => {
+test('the blog identification is called id', async () => {
+  const response = await api.get('/api/blogs')
+
+  response.body.forEach( blog => expect(blog.id).toBeDefined())
+})
+
+test('the first blog is about go to statements', async () => {
   const response = await api.get('/api/blogs')
 
   const titles = response.body.map(r => r.title)
 
   expect(titles).toContain('Go To Statement Considered Harmful')
 })
+
+test('a valid blog can be added ', async () => {
+  const newBlog = {
+    title: 'We can code',
+    author: 'Edsger W. Dijkstra',
+    url : 'http://www.u.arizona.edu/~rubinson/copyright_violations/wecancode.html',
+    likes: 8,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  const titles = response.body.map(r => r.title)
+
+  expect(response.body).toHaveLength(initialBlogs.length + 1)
+  expect(titles).toContain(
+    'We can code'
+  )
+})
+
+test('if amount of likes is not given, likes is set to 0', async () => {
+  const newBlog = {
+    title: 'We can code',
+    author: 'Edsger W. Dijkstra',
+    url : 'http://www.u.arizona.edu/~rubinson/copyright_violations/wecancode.html'
+  }
+
+  const response = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  expect(response.body.likes).toBe(0)
+})
+/*
+test('blog without content is not added', async () => {
+  const newBlog = {
+    title: 'Hello world'
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(initialBlogs.length)
+
+})*/
 
 afterAll(async () => {
   await mongoose.connection.close()
