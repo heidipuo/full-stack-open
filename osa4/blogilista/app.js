5391087b0456d80be2320f1220eae2270b060e02
Heidi <puotiniemi.heidi@gmail.com>
2023-08-21 12:52:59 +0300

    bloglist expansion done till 4.19

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -7,11 +7,10 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const morgan = require('morgan')
-
-
-
 
 const mongoUrl = config.MONGODB_URI
 
@@ -24,15 +23,15 @@ mongoose.connect(mongoUrl)
     logger.error('error connection to MongoDB:', error.message)
   })
 
-
 app.use(cors())
 app.use(express.json())
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 app.use(middleware.requestLogger)
 
-
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
