10831cffb21cfd2f73626228b67a58b763a1cf8d
Heidi <puotiniemi.heidi@gmail.com>
2023-09-01 11:05:30 +0300

    bloglist cypress tests task 5.19 and 5.20 done

--- osa5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ osa5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,14 +1,14 @@
 describe('Blog ', function() {
   beforeEach(function() {
-    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
 
     const user = {
       name: 'Test User',
       username: 'test user',
       password: 'secret'
     }
-    cy.request('POST', 'http://localhost:3003/api/users/', user)
-    cy.visit('http://localhost:5173')
+    cy.request('POST', `${Cypress.env('BACKEND')}/users`, user)
+    cy.visit('')
   })
 
   it('login form is shown', function() {
@@ -57,10 +57,41 @@ describe('Blog ', function() {
   })
   describe('when logged in', function() {
     beforeEach(function() {
-      cy.get('#username').type('test user')
-      cy.get('#password').type('secret')
-      cy.get('#login-button').click()
+     cy.login({username: 'test user', password: 'secret'})
     })
+  
+    it('a new blog can be created', function() {
+      cy.contains('Add blog').click()
+      cy.get('.title').type('a new blog')
+      cy.get('.author').type('blogger')
+      cy.get('.url').type('www.blog.fi')
+      cy.contains('create').click()
+
+      cy.get('.success')
+      .should('contain', 'You added a new blog: a new blog by blogger')
+      .and('have.css', 'color', 'rgb(0, 128, 0)')
+      cy.contains('a new blog - blogger')
+    })
+
+    describe('and a blog exists', function () {
+      beforeEach(function () {
+        cy.createBlog({
+          title: 'Another blog post',
+          author: 'Big Blogger',
+          url: 'www.com'
+        })
+      })
+
+      it.only('a blog can be liked', function() {
+        cy.contains('view').click()
+        cy.contains('like').click()
+        
+        cy. get('.blogInfo')
+          .should('contain', 1)
+          .and('not.contain', 0)
+        
+      })
   })
+})
 
 })
